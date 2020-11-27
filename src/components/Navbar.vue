94d0b669c6ab75a784b6cebda6dace141933c381
darwin3750 <damedel@mymail.mapua.edu.ph>
2020-11-27 17:25:11 +0800

    partially style navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,88 @@
 <template>
-  <nav>
-    <router-link to="/"> Home </router-link>
-    <router-link v-if="!loggedIn" to="/signup" > Sign up</router-link>
-    <a href="#" v-if="loggedIn" @click.prevent @click="logout"> Logout </a>
-    <router-link v-else to="/login" > Login </router-link>
-  </nav>
+  <b-navbar toggleable="lg" sticky class="navbar shadow">
+    <!-- The eTuro banner -->
+    <b-navbar-brand class="navbar-brand" to="/"> 
+      <img src="../assets/eTuro_banner.png" height="60px" width="auto"> 
+    </b-navbar-brand>
+    <!-- collapse button -->
+    <b-navbar-toggle target="nav-collapse">
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up" class="text-contrast border-contrast"></b-icon>
+        <b-icon v-else icon="chevron-bar-down" class="text-contrast border-contrast"></b-icon>
+      </template>
+    </b-navbar-toggle>
+    <!-- collapsible content -->
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <!-- The search bar -->
+        <div class="navbar-form">
+          <b-input-group class="w-100">
+            <b-form-input placeholder="Search from dozens of lessons, topics, and tutorials" 
+                type="text" class="form-control navbar-search"></b-form-input>
+            <b-input-group-append>
+              <button class="btn text-contrast border border-contrast">
+                <b-icon icon="search"/>
+              </button>
+            </b-input-group-append>
+          </b-input-group>
+        </div>
+
+        <!-- sign in button -->
+        <b-nav-item class="nav-link p-0" v-if="!loggedIn" to="/login">
+          <button class="text-left btn btn-lg text-contrast">
+            <b-icon icon="box-arrow-in-right"></b-icon>
+            Sign In
+          </button>
+        </b-nav-item>
+        <!-- Profile -->
+        <b-nav-item class="" v-else v-click-outside="closeProfile" @click="viewProfileMenu = true">
+          <!-- Profile button in navbar -->
+          <a href="#" class="nav-link">
+            Theme
+            <b-icon font-scale="0.6" 
+              class="align-middle" 
+              icon="caret-down-fill" 
+              variant="">
+            </b-icon>
+          </a>
+          <!-- Profile menu itself -->
+          <transition name="fade">
+            <div v-if="choosingTheme" class="shadow card p-3 position-absolute" id="theme-dropdown">
+              <a class="d-block w-100 p-2" href="#" @click="lighten">Light</a>
+              <a class="d-block w-100 p-2" href="#" @click="darken">Dark</a>
+            </div>
+          </transition>
+        </b-nav-item>
+        <!-- create acc button -->
+        <b-nav-item class="nav-link p-0" v-if="!loggedIn" to="/signup">
+          <button class="text-left btn btn-lg text-contrast">
+            <b-icon icon="person-plus"></b-icon>
+            Create an Account
+          </button>
+        </b-nav-item>
+        <!-- Notifications -->
+        <b-nav-item class="" v-else v-click-outside="closeNotifications" @click="viewNotificationsMenu = true">
+          <!-- Notifications button in navbar -->
+          <a href="#" class="nav-link">
+            Theme
+            <b-icon font-scale="0.6" 
+              class="align-middle" 
+              icon="caret-down-fill" 
+              variant="">
+            </b-icon>
+          </a>
+          <!-- Notifications menu itself -->
+          <transition name="fade">
+            <div v-if="choosingTheme" class="shadow card p-3 position-absolute" id="theme-dropdown">
+              <a class="d-block w-100 p-2" href="#" @click="lighten">Light</a>
+              <a class="d-block w-100 p-2" href="#" @click="darken">Dark</a>
+            </div>
+          </transition>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+    <!-- <b-nav-item class="nav-link" v-else to="/login">Sign Out</b-nav-item> -->
+  </b-navbar>
 </template>
 
 <script>
@@ -32,6 +110,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .navbar{
+    background-color: var(--eturo-main);
+  }
+  .navbar-form{
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+    position: relative;
+    -webkit-box-orient: horizontal;
+    -ms-flex-align: center;
+    -webkit-box-align: center;
+    align-items: center;
+    width: 100%;
+    max-width: 1000px;
+  }
+  .nav-link{
+    white-space: nowrap !important;
+  }
+  .navbar-search{
+    min-width: 400px;
+  }
 </style>
