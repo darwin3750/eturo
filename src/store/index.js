47e1e46611dfbf84e8e0c78205fec9785bb11b12
the-siegmeister <the-siegmeister@protonmail.com>
2020-11-27 16:57:07 +0800

    Store current user reference in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -8,14 +8,17 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     currentUser: auth.currentUser,
+    currentUserReference: null,
   },
   mutations: {
     toggleCurrentUser(state) {
       state.currentUser = auth.currentUser
+      state.currentUserReference = auth.currentUser ? userCollection.doc(auth.currentUser.id) : ""
     }
   },
   getters: {
     currentUser: (state) => state.currentUser,
+    currentUserReference: (state) => state.currentUserReference,
 
     // boolean conversion
     loggedIn: (state) => !!state.currentUser,
